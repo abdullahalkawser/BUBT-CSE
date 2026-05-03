c2e298eb7000f377a2107b2d5e4881b83f0c46e1
abdullahalkawser <abdullahalkawser1@gmail.com>
2026-05-04 00:31:24 +0600

    Add initial implementation of Car class with engine start functionality

---
+++ C++ Object Oriented Programming/demo.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+// Class definition
+class Car {
+public:
+    string brand;  // Attribute (Property)
+    int year;      // Attribute
+
+    // Method (Behavior)
+    void startEngine() {
+        cout << brand << " engine started! Vroom Vroom!" << endl;
+    }
+};
+
+int main() {
+    // Object তৈরি করা
+    Car myCar; 
+    myCar.brand = "Toyota";
+    myCar.year = 2024;
+
+    Car friendCar;
+    friendCar.brand = "Tesla";
+
+    myCar.startEngine();
+    friendCar.startEngine();
+
+    return 0;
+}
