8e577a327a3e198a731e07cfa7c03d36e9ab15fd
abdullahalkawser <abdullahalkawser1@gmail.com>
2026-05-11 00:36:23 +0600

    Refactor Car class to include private price attribute and add getter/setter methods for price

--- C++ Object Oriented Programming/demo.cpp
+++ C++ Object Oriented Programming/demo.cpp
@@ -3,15 +3,38 @@ using namespace std;
 
 // Class definition
 class Car {
+private:
+double price;  // Private attribute
+
+
+
 public:
     string brand;  // Attribute (Property)
     int year;      // Attribute
+    double price;  // Attribute
 
     // Method (Behavior)
     void startEngine() {
         cout << brand << " engine started! Vroom Vroom!" << endl;
         
     }
+
+    //setter method for price use for set private attribute price
+
+void setprice(double p){
+    price  =p; // this->price = p; // this pointer is used to refer to the current object's price attribute
+}
+
+
+//getter method for price  use for access private attribute price
+double getprice(){
+    return price;
+}
+
+
+
+
+
 };
 
 int main() {
